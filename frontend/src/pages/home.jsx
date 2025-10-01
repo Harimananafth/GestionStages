93df497d1943ba57a3bb603c848cd511497b1eb3
Harimananafth <harimananafth@gmail.com>
2025-10-02 00:35:07 +0300

    Création de la landingPage (50%)

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react"
+import Hero from "../components/home/hero";
+import Echantillon from "../components/home/echantillon";
+
+export default function Home(){
+
+    //Effect au scroll
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("in-view");
+            } else {
+                entry.target.classList.remove("in-view");
+            }
+            });
+        },
+        { threshold: 0.3 }
+        );
+
+        // Sélectionne tous les éléments avec ces classes
+        const targets = document.querySelectorAll(
+        ".scale, .upEntry, .leftEntry, .rightEntry"
+        );
+
+        targets.forEach((el) => observer.observe(el));
+
+        // Nettoyage quand le composant est démonté
+        return () => {
+        targets.forEach((el) => observer.unobserve(el));
+        };
+    }, []);
+
+    return (
+        <>
+            <Hero />
+            <Echantillon />
+        </>
+    )
+}
