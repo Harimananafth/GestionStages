81cff95fde39e1ac617ff180b26c5ffc98420138
Harimananafth <harimananafth@gmail.com>
2025-11-03 14:47:18 +0300

    Bugfix : variable case apiUrl in adminCharts

--- frontend/src/components/admin/dashboard/adminCharts.jsx
+++ frontend/src/components/admin/dashboard/adminCharts.jsx
@@ -5,7 +5,7 @@ const fetcher = (url) =>
   fetch(url, { credentials: "include" }).then((res) => res.json());
 
 export default function AdminCharts() {
-  const ApiUrl = import.meta.env.PROD
+  const apiUrl = import.meta.env.PROD
     ? import.meta.env.VITE_PROD_API_URL
     : import.meta.env.VITE_API_URL;
   const { data, error, isLoading } = useSWR(`${apiUrl}/stats`, fetcher);
