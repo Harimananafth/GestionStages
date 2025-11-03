6215bf00ca52988ae4765fbab693e811930e1c5d
Harimananafth <harimananafth@gmail.com>
2025-11-03 20:25:57 +0300

    Bugfix : callback, wrong variable access

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -51,7 +51,7 @@ class Auth {
         }),
         { httpOnly: true, maxAge: 10 * 60 * 1000 }
       );
-      return res.redirect(`${this.FRONTEND_URL}auth/sign-up/set-password`);
+      return res.redirect(`${Auth.FRONTEND_URL}auth/sign-up/set-password`);
     }
 
     const updates = {};
@@ -74,7 +74,7 @@ class Auth {
 
     res.cookie("token", token, Auth.cookieOptions());
     res.redirect(
-      `${this.FRONTEND_URL}auth/login-success?id=${
+      `${Auth.FRONTEND_URL}auth/login-success?id=${
         user.id
       }&email=${encodeURIComponent(user.email)}&roles=${encodeURIComponent(
         JSON.stringify(roleNames)
